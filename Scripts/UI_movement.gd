585724e21955b07a0eaa5dfe01b7d61295c99248
Vicente Ibanez <86447257+Vicente-Ibanez@users.noreply.github.com>
2024-02-10 19:09:56 -0500

    Basic Camera Movements and Angle

---
+++ Scripts/UI_movement.gd
@@ -0,0 +1,21 @@
+extends Node
+
+const MOVE_SPEED = 10
+#var left_limit = 600
+#var right_limit = 2800
+
+
+func _process(delta):
+	print_debug(self.position.z)
+	if Input.is_action_pressed("camera_forward"):
+		#if(self.position.x >= left_limit):
+		self.position.z -=  delta * MOVE_SPEED
+	elif Input.is_action_pressed("camera_back"):
+		#if(self.position.x <= right_limit):
+		self.position.z += delta * MOVE_SPEED
+	elif Input.is_action_pressed("camera_left"):
+		#if(self.position.x <= right_limit):
+		self.position.x -= delta * MOVE_SPEED
+	elif Input.is_action_pressed("camera_right"):
+		#if(self.position.x <= right_limit):
+		self.position.x += delta * MOVE_SPEED
