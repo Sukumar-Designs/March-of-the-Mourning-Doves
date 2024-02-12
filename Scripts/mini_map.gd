10fd76bf0affeb8d5cb4f25de4978978b82c5664
Vicente Ibanez <86447257+Vicente-Ibanez@users.noreply.github.com>
2024-02-11 21:40:38 -0500

    Fixing Rotation

--- Scripts/mini_map.gd
+++ Scripts/mini_map.gd
@@ -25,15 +25,25 @@ func _ready():
 func _process(delta):
 	if !player:
 		return
-	#player_marker.rotation = player.rotation.y * 1.5#+ PI / 2
+	#print_debug(player.rotation.y + PI / 2, "player")
+	#print_debug(player_marker.rotation, "marker")
+	#print_debug(player_marker.position, "position")
+	#player_marker.rotation = player.rotation.y + PI / 2
 	#player_marker.rotate(player.rotation.y * 1.5)#+ PI / 2
 	#print_debug(player_marker.rotation)
 	for item in markers:
 		if item.is_in_group("player"):
+			# Move Player Icon
 			var speed = (8.1/5) * player.speed
 			var obj_pos = speed*(Vector2(item.position.x, item.position.y) + Vector2(player.position.x, player.position.z)) * grid_scale + grid.size / 2
 			markers[item].position = obj_pos
+			
+			# Rotate Player Icon
+			print_debug(player_marker.position, "position")
+			markers[item].rotation = - item.rotation.y
+			
 		else:
+			# Move all other icons
 			var speed = (15.25/5) * item.speed 
 			var obj_pos = speed*(Vector2(item.position.x, item.position.y))* grid_scale + grid.size / 2
 			markers[item].position = obj_pos
