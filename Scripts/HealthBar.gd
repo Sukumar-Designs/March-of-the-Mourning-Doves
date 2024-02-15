b40628c0bf508f4903939f3faca9753cb270658b
Vicente Ibanez <86447257+Vicente-Ibanez@users.noreply.github.com>
2024-02-15 18:20:01 -0500

    changes

---
+++ Scripts/HealthBar.gd
@@ -0,0 +1,13 @@
+extends ProgressBar
+@export var side = "squirrel"
+@onready var towerNodeBlue = get_node('../../Blue_Tower_Full')
+@onready var towerNodeRed = get_node('../../Red_Tower_Full')
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	towerNodeBlue.connect("healthChanged", updateBar)
+	towerNodeRed.connect("healthChanged", updateBar)
+
+func updateBar(num, signal_side):
+	if signal_side == side:
+		self.value = num
