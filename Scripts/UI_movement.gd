83c8834a65fb9d11fe2d80d034bdf104a4bf5e77
Vicente Ibanez <86447257+Vicente-Ibanez@users.noreply.github.com>
2024-02-18 12:14:18 -0500

    Set boarder to prevent camera going too far

--- Scripts/UI_movement.gd
+++ Scripts/UI_movement.gd
@@ -6,7 +6,7 @@ var enemy = "squirrel"
 # Camera Movement
 var left_limit = -50
 var right_limit = 50
-var upper_limit = -58
+var upper_limit = -55
 var lower_limit = 0
 
 var speed = 5.0
@@ -22,13 +22,14 @@ func _input(event):
 			rotate_y(deg_to_rad(event.relative.x * mouse_sensativity))
 
 func _process(delta):
-	pass
+	print_debug(self.position)
 	if Input.is_action_pressed("sprint"):
 		speed=sprint
 	else:
 		speed = speed_normal
 
 func _physics_process(delta):
+		
 	var input_dir = Input.get_vector("camera_left", "camera_right", "camera_forward", "camera_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -38,3 +39,9 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 	move_and_slide()
+	
+	# Clamp player position
+	var new_position = position + velocity * delta
+	new_position.x = clamp(new_position.x, left_limit, right_limit)
+	new_position.z = clamp(new_position.z, upper_limit, lower_limit)
+	position = new_position
