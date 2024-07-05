7b8bf2c7dc5eb2dc5f04bd9ff583bac6b60d4c29
Vicente Ibanez <86447257+Vicente-Ibanez@users.noreply.github.com>
2024-07-05 17:54:57 -0400

    Adding ability to have multiple creatures float down river

--- Scripts/river.gd
+++ Scripts/river.gd
@@ -1,13 +1,17 @@
 extends Path3D
 
-@onready var path = $PathFollow3D
+#@onready var path = $PathFollow3D
 var creatures_to_push = []
 const move_speed = 1
 
+var Path_instance = PathFollow3D
+
 func _physics_process(delta):
-	if creatures_to_push.size() > 0:
-		path.progress += move_speed * delta
-		print_debug(path.progress)
+	#if creatures_to_push.size() > 0:
+	for creature in creatures_to_push:
+		creature[1].progress += move_speed * delta
+		print_debug(creature[0].position)
+		#print_debug(path.progress)
 		#for creature in creatures_to_push:
 			#if creature.get_child($MultiplayerSynchronizer).get_multiplayer_authority() == multiplayer.get_unique_id():
 		#if path.progress < 1:
@@ -22,11 +26,20 @@ func _physics_process(delta):
 func float_down_river(creature):
 	if !creatures_to_push.has(creature):
 		var old_loc = creature.position
+		print_debug(creature.position, "!!!!!")
 		creature.get_parent().remove_child(creature)
-		path.add_child(creature)
+
+		# Clear the creature collision nodes 
 		remove_features(creature)
+		
+		# Instantiate path for creature to follow
+		var path_instance = Path_instance.instance()
+		add_child(path_instance)
+		path_instance.add_child(creature)
+		
 		creature.set_location(old_loc)
-		creatures_to_push.append(creature)
+		creatures_to_push.append([creature, path_instance])
+		print_debug(creature.position, "_____")
 		#creature.position = old_loc 
 		#creature.moving = true
 		#creature.move_speed = 1.0
@@ -40,6 +53,3 @@ func remove_features(creature):
 			child.queue_free()
 
 	creature.assign_target(self)
-	#func delete_components():
-	#for child in get_children():
-		#child.queue_free()
