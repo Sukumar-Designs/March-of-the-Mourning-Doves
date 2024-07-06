7cac495817f2eef36c492cdf017f33937b48cce1
Vicente Ibanez <86447257+Vicente-Ibanez@users.noreply.github.com>
2024-07-06 08:34:04 -0400

    Float creature down the river

--- Scripts/river.gd
+++ Scripts/river.gd
@@ -6,7 +6,6 @@ const move_speed = 4
 var Path_instance = PathFollow3D
 const PATH_LENGTH = 218.0
 
-
 func _physics_process(delta):
 	var to_remove = []
 	var spot = 0
@@ -17,7 +16,6 @@ func _physics_process(delta):
 			to_remove.append(spot)
 		else:
 			creature[1].progress += move_speed * delta
-			#print_debug(creature[0].position)
 		spot+=1
 			
 	for index in to_remove:
@@ -30,30 +28,26 @@ func float_down_river(creature):
 		
 		# Clear the creature collision nodes 
 		remove_features(creature)
-		
-		print_debug(creature.position, "!!!!!")
 		creature.get_parent().remove_child(creature)
 		
 		# Instantiate path for creature to follow
 		var path_instance = Path_instance.new()
 		add_child(path_instance)
 		path_instance.add_child(creature)
-		#path_instance.progress = old_loc.x - PATH_LENGTH
-		creature.set_location(old_loc)
-		print_debug(old_loc.x, "  ", path_instance.progress, "!!!!!!!!!!!!!!")
-		#creature.set_location(old_loc)
+		creature.set_global_position(old_loc)
+		path_instance.progress = get_offset(path_instance, creature.position)
 		creatures_to_push.append([creature, path_instance])
-		print_debug(creature.position, "_____")
-		#creature.position = old_loc 
-		#creature.moving = true
-		#creature.move_speed = 1.0
 		
+
+
+func get_offset(path_instance, local_space):
+	return curve.get_closest_offset(to_local(local_space))
+
 
 func remove_features(creature):
 	creature.set_collision_mask_value(4, false) 
 	for child in creature.get_children():
 		if child is CollisionShape3D or child is Area3D:
-			print_debug("FREEZING", child)
 			child.queue_free()
 
 	creature.assign_target(self)
